854e4b3794fb526bf0d7bd732621c7257c3c474e
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-01-23 11:02:00 +0100

    arrays serialized and deserialized

--- app/src/main/java/pl/apka/sharedpreferences/MainActivity.java
+++ app/src/main/java/pl/apka/sharedpreferences/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,15 +18,41 @@ public class MainActivity extends AppCompatActivity {
 
         SharedPreferences sharedPreferences = this.getSharedPreferences("pl.apka.sharedpreferences", Context.MODE_PRIVATE);
 
+        
         ArrayList<String> friends = new ArrayList<>();
 
         friends.add("Jan");
         friends.add("Ola");
         friends.add("Ala");
+        friends.add("Michał");
+        friends.add("Grześ");
+        friends.add("Jan");
+        friends.add("Ola");
+        friends.add("Ala");
+        friends.add("Michał");
+        friends.add("Grześ");
+
+        try {
+            String friendsStr = ObjectSerializer.serialize(friends);
+            sharedPreferences.edit().putString("friends",friendsStr).apply();
+
+            Log.e("FRIENDS",friendsStr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
+        ArrayList<String> newFriends = new ArrayList<>();
 
+        try {
+            newFriends = (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("friends", ObjectSerializer.serialize(new ArrayList<String>())));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+        for (String friend : newFriends) {
+            Log.e("NEW FRIENDS", friend);
+        }
         //sharedPreferences.edit().putString("username","Jan").apply();
 
         //String username = sharedPreferences.getString("username","");
